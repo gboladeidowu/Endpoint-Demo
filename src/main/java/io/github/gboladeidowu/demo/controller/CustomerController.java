85fb0c5872ec7a9ad1aa9fe53ba145ad80bf6148
Gbolade <idowugboladeshem@gmail.com>
2025-01-30 02:56:52 +0100

    Endpoint demo

--- src/main/java/io/github/gboladeidowu/demo/controller/CustomerController.java
+++ src/main/java/io/github/gboladeidowu/demo/controller/CustomerController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/test/customer")
+@RequestMapping("/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
